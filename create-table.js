9e6c2292c2c08adada31203b2a8c7367d10917e7
Emerson Ferreira <emecatarino@yahoo.com.br>
2022-04-04 15:22:45 -0300

    Ajustes de configuracao

--- create-table.js
+++ create-table.js
@@ -1,48 +1,53 @@
+// defined use database with import of module mysql 
 const mysql = require('mysql')
-const pw = require('./env') // arquivo que armazena senhas
+const pw = require('./env') // file settings passwords
 
+// create connection
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: 'root',
-    password: pw.password, // coloque aqui a senha de seu BD
+    user: 'root', // defined where your user
+    password: pw.password, // use where your password 
     database: 'nodemysql'
 })
 
+// establish connection with database MySql   
 connection.connect(function (err) {
     if (err) return console.log(err)
     console.log('Conexão realizada com sucesso')
     createTable(connection)
-    addRows(connection)
-    connection.end() //fecha a conexão
 })
 
 console.log('Aguarde...')
 
-function createTable(conn){
+// create table
+function createTable(conn) {
 
-    const sql = "CREATE TABLE IF NOT EXISTS clientes ("+
-                "id int NOT NULL AUTO_INCREMENT,"+
-                "nome varchar(150) NOT NULL,"+
-                "cpf char(11) NOT NULL,"+
-                "PRIMARY KEY (id)"+
-                ");";
-    
-    conn.query(sql, function (error, results, fields){
-        if(error) return console.log(error);
+    const sql = "CREATE TABLE IF NOT EXISTS clientes (" +
+        "id int NOT NULL AUTO_INCREMENT," +
+        "nome varchar(150) NOT NULL," +
+        "cpf char(11) NOT NULL," +
+        "PRIMARY KEY (id)" +
+        ");";
+
+    conn.query(sql, function (error, results, fields) {
+        if (error) return console.log(error);
         console.log('Tabela clientes criada com sucesso!');
+        addRows(connection)
+        conn.end() // close connection
     });
 }
 
-function addRows(conn){
-  const sql = "INSERT INTO Clientes(nome, cpf) VALUES ?";
-  const values = [
+// load registers
+function addRows(conn) {
+    const sql = "INSERT INTO Clientes(nome, cpf) VALUES ?";
+    const values = [
         ['Sofia Almeida', '12345678901'],
         ['Francisco Ferreira', '09876543210'],
         ['Emerson Catarino', '12312312399']
-      ];
-  conn.query(sql, [values], function (error, results, fields){
-          if(error) return console.log(error);
-          console.log('Registros adicionados com sucesso!');
-      });
+    ];
+    conn.query(sql, [values], function (error, results, fields) {
+        if (error) return console.log(error);
+        console.log('Registros adicionados com sucesso!');
+    });
 }
